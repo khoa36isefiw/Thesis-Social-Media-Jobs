66ac7cf704f0cf28b470f9f33e294f2e9f939ff7
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-04-01 22:33:03 +0700

    add scroll to top and background image

--- client/src/components/UserInformation/UserInformation.js
+++ client/src/components/UserInformation/UserInformation.js
@@ -1,35 +1,74 @@
 import React from 'react';
 import { Box, Typography, Divider, Avatar } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import UserAvatar from '../../assets/images/avatar.jpeg';
+import { blue } from '@mui/material/colors';
+import { scrollToTop } from '../ScrollToTop/ScrollToTop';
 function UserInformation() {
+    const navigate = useNavigate();
+    const handleNavigateToProfile = () => {
+        navigate('/user-profile');
+        scrollToTop();
+    };
     return (
-        <Box>
-            <Box>
+        <Box sx={{ border: '1px solid #d9d9d9', backgroundColor: '#fff', borderRadius: '12px' }}>
+            {/* <Box>
                 <Avatar
                     src={UserAvatar}
                     alt="User Avatar"
                     sx={{ m: 'auto', mb: 2, height: '64px', width: '64px' }}
+                /> */}
+
+            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <Avatar
+                    // src={UserBackgroundImage}
+                    src={
+                        'https://media.istockphoto.com/id/835370890/photo/sunset-sunrise-with-clouds-light-rays-and-other-atmospheric-effect.jpg?s=612x612&w=0&k=20&c=zGDOBYVFY74wX2gUgkonYGtNl1zenev5mPotAqUlJbM='
+                    }
+                    alt="User Backgorund Image"
+                    sx={{
+                        borderTopLeftRadius: '12px',
+                        borderTopRightRadius: '12px',
+                        borderBottomLeftRadius: '0',
+                        borderBottomRightRadius: '0',
+                        width: '100%',
+                        height: '80px',
+                        objectFit: 'contain',
+                        zIndex: 2,
+                    }}
                 />
-                <Link to="/user-profile">
-                    <Typography
-                        sx={{
-                            fontSize: '16px',
-                            fontWeight: 'bold',
-                            mt: '4px',
-                            '&:hover': {
-                                cursor: 'pointer',
-                                textDecoration: 'underline',
-                            },
-                        }}
-                    >
-                        Huynh Dang Khoa
-                    </Typography>
-                </Link>
+                <Avatar
+                    src={UserAvatar}
+                    alt="User Avatar"
+                    sx={{
+                        height: '80px',
+                        width: '80px',
+                        mt: '-40px',
+                        border: '4px solid #fff',
+                        zIndex: 99,
+                    }}
+                />
+
+                <Typography
+                    onClick={handleNavigateToProfile}
+                    sx={{
+                        fontSize: '16px',
+                        fontWeight: 'bold',
+
+                        mt: '4px',
+                        '&:hover': {
+                            cursor: 'pointer',
+                            color: blue[800],
+                            textDecoration: 'underline',
+                        },
+                    }}
+                >
+                    Huynh Dang Khoa
+                </Typography>
                 <Typography sx={{ fontSize: '14px', mb: '8px' }}>Student at HCMUTE</Typography>
-                <Divider />
+                <Box sx={{ width: '100%', height: '1px', bgcolor: '#d9d9d9' }}></Box>
             </Box>
-            <Box sx={{ mt: 2, mb: 2 }}>
+            <Box sx={{ p: 2 }}>
                 <Typography
                     sx={{ fontSize: '14px', fontWeight: 'bold', mt: '4px', textAlign: 'left' }}
                 >
@@ -38,7 +77,6 @@ function UserInformation() {
                 <Typography sx={{ fontSize: '14px', mb: 2, textAlign: 'left' }}>
                     Connect with alumina
                 </Typography>
-                <Divider />
             </Box>
         </Box>
     );
