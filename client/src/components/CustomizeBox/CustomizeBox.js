67769f40b58584da1f69c2be6fe99e5f60062831
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-04-01 22:42:07 +0700

    define box component

---
+++ client/src/components/CustomizeBox/CustomizeBox.js
@@ -0,0 +1,9 @@
+import { Box, styled } from '@mui/material';
+export const CustomizeBox = styled(Box)(({}) => ({
+    minHeight: '50px',
+    width: '100%',
+    backgroundColor: '#fff',
+    border: '1px solid #d9d9d9',
+    borderRadius: '12px',
+    padding: '16px',
+}));
