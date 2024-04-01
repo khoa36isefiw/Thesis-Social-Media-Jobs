7da6fc5d7cb5fc8803e3a2e5e986429b53b05e86
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-04-01 22:38:54 +0700

    add some component for this layout

--- client/src/layouts/UserProfileLayout/UserProfileLayout.js
+++ client/src/layouts/UserProfileLayout/UserProfileLayout.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { styled, Paper, Container } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import Advertising from '../../components/Advertising/Advertising';
 import Header from '../DefaultLayouts/Header/Header';
-import Footer from '../DefaultLayouts/Footer/Footer';
+import SimilarFollowers from './SimilarFollowers/SimilarFollowers';
+import HiringCareer from '../../components/Advertising/HiringCareer';
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -19,19 +20,24 @@ function UserProfileLayout({ children }) {
         <Box sx={{ backgroundColor: '#f3f2f0' }}>
             <Header />
             <Container sx={{ mt: 12 }}>
-                <Grid container rowSpacing={3} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                    <Grid item xs={9}>
-                        <Box sx={{ minHeight: '10vh', borderRadius: '24px' }}>
-                            <Box>{children}</Box>
-                        </Box>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Box>
+                <Box>
+                    <Grid container rowSpacing={3} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                        <Grid item xs={9}>
+                            <Box sx={{ minHeight: '10vh', borderRadius: '24px' }}>
+                                <Box>{children}</Box>
+                            </Box>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <HiringCareer />
+                            <SimilarFollowers />
                             <Advertising />
-                        </Box>
-                        <Footer />
+                        </Grid>
                     </Grid>
-                </Grid>
+                    <Typography sx={{ fontSize: '14px', py: 2, color: 'text.secondary' }}>
+                        Aikotoba Corporation &copy; 2023
+                    </Typography>
+                    {/* </Container> */}
+                </Box>
             </Container>
         </Box>
     );
