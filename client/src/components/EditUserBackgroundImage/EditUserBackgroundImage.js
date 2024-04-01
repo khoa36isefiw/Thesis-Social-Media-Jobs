25361f8f7de37f76f451ebf145d2409c7ceb1779
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-04-01 22:42:40 +0700

    make UI for user edits background Image

---
+++ client/src/components/EditUserBackgroundImage/EditUserBackgroundImage.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import { Box, IconButton, Typography, Button, Divider, Avatar } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import DefaultBackgroundImage from '../../assets/images/pn.jpeg';
+import { CustomizeTypography } from '../CustomizeTypography/CustomizeTypography';
+function EditUserBackgroundImage({ imgUrl, handleClose }) {
+    return (
+        <Box
+            sx={{
+                position: 'relative',
+                backgroundColor: '#fff',
+                width: '50%',
+                minHeight: '300px',
+                margin: 'auto',
+                mt: '64px',
+                borderRadius: '8px',
+                boxShadow: '0 4px 4px #333',
+                //  close icon doesn't overflow
+                overflow: 'hidden',
+            }}
+        >
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    p: 1,
+                }}
+            >
+                <CustomizeTypography fs={'20px'} fw={true}>
+                    Add background photo
+                </CustomizeTypography>
+                <IconButton
+                    disableFocusRipple
+                    sx={{
+                        '&:hover': {
+                            backgroundColor: '#d9d9d9',
+                        },
+                    }}
+                    onClick={handleClose}
+                >
+                    <CloseIcon fontSize="large" />
+                </IconButton>
+            </Box>
+
+            <Divider sx={{ mb: 2 }} />
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}
+            >
+                <Avatar
+                    src={DefaultBackgroundImage}
+                    alt="Default User Background Image"
+                    sx={{ height: '256px', width: '256px' }}
+                />
+                <Box sx={{ width: '60%', textAlign: 'center', p: 1 }}>
+                    <CustomizeTypography fs="18px" sx={{ color: '#191919' }}>
+                        Showcase your personality, interests, team moments or notable milestones
+                    </CustomizeTypography>
+                    <CustomizeTypography fs="13px" sx={{ color: 'text.primary', mt: 2 }}>
+                        A good background photo will help you stand out.
+                    </CustomizeTypography>
+                </Box>
+            </Box>
+            <Divider sx={{ mt: 2, mb: 2 }} />
+            <Box sx={{ display: 'flex', alignItems: 'flex-end', justifyContent: 'flex-end' }}>
+                <Button
+                    variant="contained"
+                    sx={{
+                        fontSize: '14px',
+                        textTransform: 'initial',
+                        fontWeight: 'bold',
+                        width: '200px',
+                        borderRadius: '24px',
+                        mb: 2,
+                        mx: 2,
+                        mr: '20px',
+                    }}
+                >
+                    Edit profile background
+                </Button>
+            </Box>
+        </Box>
+    );
+}
+
+export default EditUserBackgroundImage;
