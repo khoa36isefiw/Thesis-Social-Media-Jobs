26727704bf8b5ef873be86cdcc52168b99b9d5ab
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-04-01 22:30:56 +0700

    make new styles

--- client/src/components/UserGroup/UserGroup.js
+++ client/src/components/UserGroup/UserGroup.js
@@ -2,47 +2,51 @@ import React from 'react';
 import { Box, Typography, Divider } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { CustomizeTypography } from '../CustomizeTypography/CustomizeTypography';
-
+import { CustomizeBox } from '../CustomizeBox/CustomizeBox';
 
 function UserGroup() {
     return (
-        <Box>
-            <Typography
-                sx={{
-                    fontSize: '14px',
-                    color: 'text.secondary',
-                    fontWeight: 'bold',
-                    mt: 1,
-                    textAlign: 'left',
-                }}
-            >
-                Recent
-            </Typography>
-            <Link to="#">
+        <Box sx={{ border: '1px solid #d9d9d9', bgcolor: '#fff', borderRadius: '12px' }}>
+            <Box p={2}>
                 <Typography
                     sx={{
                         fontSize: '14px',
-                        color: '#0A66C2',
+                        color: 'text.secondary',
                         fontWeight: 'bold',
                         mt: 1,
-                        mb: 1,
                         textAlign: 'left',
-                        '&:hover': {
-                            textDecoration: 'underline',
-                        },
                     }}
                 >
-                    Groups
+                    Recent
                 </Typography>
-            </Link>
+                <Link to="#">
+                    <Typography
+                        sx={{
+                            fontSize: '14px',
+                            color: '#0A66C2',
+                            fontWeight: 'bold',
+                            mt: 1,
+                            mb: 1,
+                            textAlign: 'left',
+                            '&:hover': {
+                                textDecoration: 'underline',
+                            },
+                        }}
+                    >
+                        Groups
+                    </Typography>
+                </Link>
+            </Box>
             <Divider />
-            <Link to="#">
-                <Typography
-                    sx={{ fontSize: '14px', color: 'text.secondary', fontWeight: '600', mt: 1 }}
-                >
-                    Discover more
-                </Typography>
-            </Link>
+            <Box sx={{ display: 'flex', alignItems: 'center', p: 1, justifyContent: 'center' }}>
+                <Link to="#">
+                    <Typography
+                        sx={{ fontSize: '14px', color: 'text.secondary', fontWeight: '600', mt: 1 }}
+                    >
+                        Discover more
+                    </Typography>
+                </Link>
+            </Box>
         </Box>
     );
 }
