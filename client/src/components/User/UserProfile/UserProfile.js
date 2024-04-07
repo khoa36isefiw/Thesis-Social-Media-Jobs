0b0628ec6daf8f6377b634f2bb052ec737d56a22
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-04-07 21:07:24 +0700

    separate UserProfile to many components

--- client/src/components/User/UserProfile/UserProfile.js
+++ client/src/components/User/UserProfile/UserProfile.js
@@ -13,26 +13,20 @@ import {
     MenuList,
     ListItemIcon,
     ListItemText,
-    Tabs,
-    Tab,
-    Divider,
     Modal,
 } from '@mui/material';
-
 import { blue } from '@mui/material/colors';
 import PhotoCameraIcon from '@mui/icons-material/PhotoCamera';
 import EditNoteIcon from '@mui/icons-material/EditNote';
 import SendIcon from '@mui/icons-material/Send';
-import AddIcon from '@mui/icons-material/Add';
-
 import SaveAltIcon from '@mui/icons-material/SaveAlt';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import UserAvatar from '../../../assets/images/avatar.jpeg';
-import FPTCop from '../../../assets/images/fpt_logo.png';
-import HCMUTELogo from '../../../assets/images/hcmute.jpeg';
 import BackgroundImageModal from '../../BackgroundImageModal/BackgroundImageModal';
 import EditUserBackgroundImage from '../../EditUserBackgroundImage/EditUserBackgroundImage';
+import EditUserProfile from '../../EditUserProfile/EditUserProfile';
 
+// define
 const CustomizeTypography = styled(Typography)(({ fontSize, isBold = false }) => ({
     fontSize: fontSize || '16px',
     fontWeight: isBold ? 'bold' : 'normal',
@@ -48,54 +42,18 @@ const CustomizeButton = styled(Button)(({}) => ({
 
 // define button following by schools and companies
 
-const FollowButton = () => {
-    return (
-        <Button
-            variant="outlined"
-            sx={{ borderRadius: '24px', mt: '2px', mt: 1 }}
-            startIcon={<AddIcon sx={{ color: '#808080' }} />}
-        >
-            <Typography
-                sx={{
-                    fontSize: '12.5px',
-                    textTransform: 'capitalize',
-                    fontWeight: 'bold',
-                    color: 'gray',
-                    px: 1,
-                }}
-            >
-                Follow
-            </Typography>
-        </Button>
-    );
-};
-
-function UserProfile() {
-    return (
-        <Box>
-            <ProfileRegion />
-            <AboutRegion />
-            <EducationRegion />
-            <InterestRegion />
-        </Box>
-    );
-}
-
-export default UserProfile;
-
 const moreActionLists = [
     { icon: <SendIcon sx={{ fontSize: '20px' }} />, actionText: 'Send profile in a message' },
     { icon: <SaveAltIcon sx={{ fontSize: '20px' }} />, actionText: 'Save to PDF' },
 ];
 
 // User information section
-function ProfileRegion() {
+export function UserProfile() {
     const navigate = useNavigate();
     const [activeModal, setActiveModal] = useState(null);
     const [anchorEl, setAnchorEl] = React.useState(null);
+    const [isUserProfileOpen, setIsUserProfileOpen] = useState(false);
 
-    // const [openModal, setOpenModal] = useState(false);
-    // const [anchorEl, setAnchorEl] = React.useState(null);
     const open = Boolean(anchorEl);
     const handleClick = (event) => {
         setAnchorEl(event.currentTarget);
@@ -104,17 +62,20 @@ function ProfileRegion() {
         setAnchorEl(null);
     };
 
-    const handleImageClick = () => {
-        // setOpenModal(true);
-    };
-
     const handleOpenModal = (modalType) => {
         setActiveModal(modalType);
     };
 
     const handleCloseModal = () => {
-        // setOpenModal(false);
         setActiveModal(null);
+    };
+
+    const handleOpenUserProfile = () => {
+        setIsUserProfileOpen(true);
+    };
+
+    const handleCloseUserProfile = () => {
+        setIsUserProfileOpen(false);
     };
 
     return (
@@ -198,15 +159,17 @@ function ProfileRegion() {
                 <IconButton
                     disableTouchRipple
                     sx={{
-                        marginRight: 1,
+                        mr: 3,
                         '&:hover': {
                             backgroundColor: 'transparent',
                         },
                     }}
+                    // onClick={() => handleOpenModal('editUserProfile')}
+                    onClick={handleOpenUserProfile}
                 >
                     <EditNoteIcon
                         sx={{
-                            fontSize: '32px',
+                            fontSize: '28px',
                         }}
                     />
                 </IconButton>
@@ -275,11 +238,6 @@ function ProfileRegion() {
                     </Box>
 
                     {/* connections --> Link to network tab */}
-                    {/* <Link to="/network">
-                        <CustomizeTypography fontSize="14px" sx={{ mt: 1 }}>
-                            2 connections
-                        </CustomizeTypography>
-                    </Link> */}
                     <CustomizeTypography
                         fontSize="14px"
                         sx={{
@@ -386,325 +344,10 @@ function ProfileRegion() {
             <Modal open={activeModal === 'editUserBackground'} onClose={handleCloseModal}>
                 <EditUserBackgroundImage handleClose={handleCloseModal} />
             </Modal>
-        </Box>
-    );
-}
 
-function AboutRegion() {
-    return (
-        <Box
-            sx={{
-                minHeight: '50px',
-                width: '100%',
-                borderRadius: '12px',
-                border: '1px solid #d9d9d9',
-                backgroundColor: '#fff',
-                p: 3,
-                mt: 2,
-                mb: 2,
-            }}
-        >
-            <CustomizeTypography fontSize="20px" isBold={true}>
-                About
-            </CustomizeTypography>
-            <CustomizeTypography fontSize={'14px'} sx={{ textAlign: 'justify' }}>
-                As a backend intern, I aim to improve skills such as designing solutions for
-                software features, writing high-quality code to implement software features or fix
-                bugs, as well as performing unit testing. Through these efforts, I intend to acquire
-                specialized knowledge that can be applied to the company's corresponding business
-                needs, thereby assisting the company in future projects. My long-term goal is to
-                advance to the role of Backend Developer within three years and, further down the
-                road, to become a Fullstack Developer.
-            </CustomizeTypography>
-        </Box>
-    );
-}
+            {/* Open modal to edit user profile/ information */}
 
-function EducationRegion() {
-    return (
-        <Box
-            sx={{
-                minHeight: '50px',
-                width: '100%',
-                borderRadius: '12px',
-                border: '1px solid #d9d9d9',
-                backgroundColor: '#fff',
-                p: 3,
-                mt: 2,
-                mb: 2,
-            }}
-        >
-            <CustomizeTypography fontSize="20px" isBold={true}>
-                Education
-            </CustomizeTypography>
-            <Box
-                sx={{
-                    mt: 1,
-                    display: 'flex',
-                    alignItems: 'flex-start',
-                    justifyContent: 'flex-start',
-                }}
-            >
-                <Avatar
-                    // src={UserBackgroundImage}
-                    src={
-                        'https://media.istockphoto.com/id/835370890/photo/sunset-sunrise-with-clouds-light-rays-and-other-atmospheric-effect.jpg?s=612x612&w=0&k=20&c=zGDOBYVFY74wX2gUgkonYGtNl1zenev5mPotAqUlJbM='
-                    }
-                    alt="University Logo"
-                    sx={{
-                        borderRadius: '0px',
-                        width: '40px',
-                        height: '40px',
-                        objectFit: 'contain',
-                        zIndex: 2,
-                    }}
-                />
-
-                <Box sx={{ ml: 1 }}>
-                    <CustomizeTypography
-                        sx={{
-                            fontWeight: '600',
-                            '&:hover': {
-                                cursor: 'pointer',
-                                textDecoration: 'underline',
-                            },
-                        }}
-                        fontSize={'16px'}
-                    >
-                        HCMC University of Technology and Education
-                    </CustomizeTypography>
-                    <CustomizeTypography fontSize={'14px'}>
-                        Information Technology
-                    </CustomizeTypography>
-                    <CustomizeTypography fontSize={'14px'}>Grade: 3.06</CustomizeTypography>
-                </Box>
-            </Box>
-        </Box>
-    );
-}
-
-const TabPanel = ({ children, value, index, ...other }) => (
-    <div
-        role="tabpanel"
-        hidden={value !== index}
-        id={`tabpanel-${index}`}
-        aria-labelledby={`tab-${index}`}
-        {...other}
-    >
-        {value === index && <Box p={0}>{children}</Box>}
-    </div>
-);
-
-function InterestRegion() {
-    const [value, setValue] = React.useState(0);
-
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
-
-    return (
-        <Box
-            sx={{
-                minHeight: '25px',
-                width: '100%',
-                borderRadius: '12px',
-                border: '1px solid rgba(0, 0, 0, 0.1)',
-                backgroundColor: '#fff',
-                px: 3,
-                py: 1,
-                my: 2,
-            }}
-        >
-            <Tabs
-                value={value}
-                onChange={handleChange}
-                aria-label="Tabs example"
-                sx={{ borderBottom: '1px solid #333' }}
-            >
-                <Tab
-                    sx={{
-                        '&:hover': {
-                            backgroundColor: 'rgba(0, 0, 0, 0.1)',
-                        },
-                    }}
-                    label={
-                        <Typography sx={{ fontSize: '14px', textTransform: 'capitalize' }}>
-                            Companies
-                        </Typography>
-                    }
-                />
-                <Tab
-                    sx={{
-                        '&:hover': {
-                            backgroundColor: 'rgba(0, 0, 0, 0.1)',
-                        },
-                    }}
-                    label={
-                        <Typography sx={{ fontSize: '14px', textTransform: 'capitalize' }}>
-                            Schools
-                        </Typography>
-                    }
-                />
-            </Tabs>
-            <TabPanel value={value} index={0}>
-                {/* Tab 1 Content */}
-                <FollowCompany />
-            </TabPanel>
-            <TabPanel value={value} index={1}>
-                <FollowSchool />
-            </TabPanel>
-        </Box>
-    );
-}
-
-function FollowCompany() {
-    return (
-        <Box sx={{ display: 'flex', alignItems: 'center', my: 1, mt: 2, mx: 2 }}>
-            <img src={FPTCop} style={{ height: '30px', width: '30px', objectFit: 'contain' }} />
-            <Box sx={{ ml: 2 }}>
-                <Typography
-                    sx={{ fontWeight: 'bold', color: 'text.secondary', fontSize: '13.5px' }}
-                >
-                    FPT Information System
-                </Typography>
-                <Typography sx={{ color: 'text.secondary', fontSize: '12.5px' }}>
-                    6,969,696 followers
-                </Typography>
-                {/* <Button variant="outlined" sx={{ borderRadius: '24px', p: '4px', mt: '2px' }}>
-                    <PersonAddIcon />
-                    <Typography
-                        sx={{
-                            fontSize: '12.5px',
-                            textTransform: 'capitalize',
-                            fontWeight: 'bold',
-                            ml: 1,
-                        }}
-                    >
-                        Follow
-                    </Typography>
-                </Button> */}
-                <FollowButton />
-            </Box>
-        </Box>
-    );
-}
-
-const schools = [
-    {
-        schoolImgSrc:
-            'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Logo_Hust.png/1200px-Logo_Hust.png',
-        schoolName: 'Hanoi University of Science and Technology',
-        schoolFollowers: '35,822',
-    },
-    {
-        schoolImgSrc: HCMUTELogo,
-        schoolName: 'HCMC University of Technology and Education',
-        schoolFollowers: '6,969',
-    },
-];
-function FollowSchool() {
-    const [showMore, setShowMore] = useState(false);
-    const toggleShowMore = () => {
-        setShowMore(!showMore);
-    };
-    return (
-        <Box>
-            <Box
-                sx={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'space-between',
-                    cursor: 'pointer',
-                    my: 1,
-                    mt: 2,
-                    mx: 2,
-                }}
-            >
-                {schools.map((school, index) => (
-                    <Box sx={{ display: 'flex' }} key={index}>
-                        <Box>
-                            <img
-                                src={school.schoolImgSrc}
-                                style={{ height: '50px', width: '50px', objectFit: 'contain' }}
-                            />
-                        </Box>
-                        <Box sx={{ ml: 2 }}>
-                            <Typography
-                                sx={{
-                                    fontWeight: 'bold',
-                                    color: 'text.secondary',
-                                    fontSize: '13.5px',
-                                    '&:hover': {
-                                        textDecoration: 'underline',
-                                        color: blue[700],
-                                        cursor: 'pointer',
-                                    },
-                                }}
-                            >
-                                {/* HCMC University of Technology and Education */}
-                                {school.schoolName}
-                            </Typography>
-                            <Typography sx={{ color: 'text.secondary', fontSize: '13px' }}>
-                                {school.schoolFollowers} followers
-                            </Typography>
-                            {/* <Button
-                                variant="outlined"
-                                sx={{ borderRadius: '24px', p: '4px', mt: '2px' }}
-                            >
-                                <PersonAddIcon />
-                                <Typography
-                                    sx={{
-                                        fontSize: '12.5px',
-                                        textTransform: 'capitalize',
-                                        fontWeight: 'bold',
-                                        ml: 1,
-                                    }}
-                                >
-                                    Follow
-                                </Typography>
-                            </Button> */}
-
-                            {/* follower button */}
-                            {/* <Button
-                                variant="outlined"
-                                sx={{ borderRadius: '24px', mt: '2px' }}
-                                startIcon={<AddIcon sx={{ color: '#808080' }} />}
-                            >
-                                <Typography
-                                    sx={{
-                                        fontSize: '12.5px',
-                                        textTransform: 'capitalize',
-                                        fontWeight: 'bold',
-                                        color: 'gray',
-                                        px: 1,
-                                    }}
-                                >
-                                    Follow
-                                </Typography>
-                            </Button> */}
-                            <FollowButton />
-                        </Box>
-                    </Box>
-                ))}
-                {/* show more button */}
-            </Box>
-            <Divider sx={{ mt: 3 }} />
-            <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                {schools.length >= 2 && !showMore && (
-                    <Button
-                        variant="text"
-                        onClick={toggleShowMore}
-                        sx={{
-                            textTransform: 'capitalize',
-                            fontSize: '14px',
-                            fontWeight: 'bold',
-                        }}
-                        fullWidth={true}
-                    >
-                        show more
-                    </Button>
-                )}
-            </Box>
+            <EditUserProfile isOpen={isUserProfileOpen} handleClose={handleCloseUserProfile} />
         </Box>
     );
 }
