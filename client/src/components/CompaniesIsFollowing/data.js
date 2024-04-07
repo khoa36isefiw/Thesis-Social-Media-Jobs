9e5a423a12f0f7aeee14062ad42a6ac5e357f840
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-04-07 21:09:24 +0700

    make Interests Detail Page

---
+++ client/src/components/CompaniesIsFollowing/data.js
@@ -0,0 +1,40 @@
+export const companiesData = [
+    {
+        avatar: 'https://media.licdn.com/dms/image/C560BAQGEwS8YjHJ-Fg/company-logo_200_200/0/1631347731327?e=1720656000&v=beta&t=Ra5yfG1sSXDrC4ODVr4LAg7fkaAU6bapcWlOqoBSffg',
+        name: 'Gimasys Co., Ltd',
+        numberOfFollowers: '1,740',
+    },
+    {
+        avatar: 'https://media.licdn.com/dms/image/C560BAQG1D468Lw-C0Q/company-logo_200_200/0/1638156211709/fpt_information_system_logo?e=1720656000&v=beta&t=oVMl17YBbHx-vuJsoh_oxDxNlossD4KNoQIiBwo71K8',
+        name: 'FPT Information System',
+        numberOfFollowers: '12,622',
+    },
+    {
+        avatar: 'https://media.licdn.com/dms/image/C4D0BAQF_Sk_uSPkkyQ/company-logo_200_200/0/1680178586116?e=1720656000&v=beta&t=PEbeMO45vu0NHe2pKQ0CsUfPKBa88viofZMNjUskKeI',
+        name: 'Q Blockchain',
+        numberOfFollowers: '1,086',
+    },
+    {
+        avatar: 'https://media.licdn.com/dms/image/C4D0BAQGV_B0uVb2MOQ/company-logo_200_200/0/1656982864099?e=1720656000&v=beta&t=_1RaALRF8yaGokkO25h1n75yEeoyyIvdwG2Lsf1USZM',
+        name: 'Rekrutmen Bersama BUMN',
+        numberOfFollowers: '1,029,098',
+    },
+];
+
+export const schoolData = [
+    {
+        avatar: 'https://media.licdn.com/dms/image/C560BAQHGnx_4t6NIjw/company-logo_200_200/0/1631376203871?e=1720656000&v=beta&t=iKMWshTXLpWfQlT9cL3amkwcjoCWD13aXe7OpuQ_nCY',
+        name: 'University of Information and Technology',
+        numberOfFollowers: '20,678',
+    },
+    {
+        avatar: 'https://media.licdn.com/dms/image/C4D0BAQHAUZMBSNi7uw/company-logo_200_200/0/1631353339911?e=1720656000&v=beta&t=17WKzpv0RvdhGe9R3mf631Doea08JJmFwuhXGzyV01s',
+        name: 'Bach Khoa University',
+        numberOfFollowers: '13,622',
+    },
+    {
+        avatar: 'https://media.licdn.com/dms/image/C510BAQEaVrl7oCuRsg/company-logo_200_200/0/1630584236544/hcmutespkt_logo?e=1720656000&v=beta&t=e87G05MoI4uAX3l1j0xXUvUnZUOOR0bQppWDwPkoz5A',
+        name: 'HCMC University of Technology and Education ',
+        numberOfFollowers: '15,086',
+    },
+];
