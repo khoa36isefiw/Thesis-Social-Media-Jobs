8793edfdb5a67e4eca1360f1f6978ed223c8e319
Khoa_HD <huynhdangkhoa961617@gmail.com>
2024-04-19 20:51:48 +0700

    re-define breakpoints

--- client/src/components/Theme/Theme.js
+++ client/src/components/Theme/Theme.js
@@ -9,14 +9,33 @@ export const theme = createTheme({
         detailTextColor: '#2196f3',
     },
     breakpoints: {
+        // values: {
+        //     mobile: 739,
+        //     tablet: 740,
+        //     ipadPro: 1023,
+        //     desktop: 1024,
+        // },
         values: {
-            mobile: 0,
-            tablet: 768,
-            ipadPro: 1024,
-            laptop: 1200,
-            desktop: 1600,
-            // laptop: 1024,
-            // desktop: 1200,
+            xs: 0,
+            sm: 739,
+            md: 740,
+            lg: 1023,
+            xl: 1024,
+
+            // sm: 0,
+            // xs: 739,
+            // md: 740,
+            // lg: 1023,
         },
     },
 });
+
+// // Define a variable for the media query condition
+// const mobileScreen = '@media only screen and (max-width: 46.1875em)';
+// const tabletScreen = '@media only screen between (min-width: 46.25em) and (max-width:63.9375em)';
+// const desktopScreen = '@media only screen and (min-width: 64em)';
+
+export const mobileScreen = `@media only screen and (max-width: ${theme.breakpoints.values.sm}px)`;
+export const tabletScreen = `@media only screen and (min-width: ${theme.breakpoints.values.md}px) and (max-width: ${theme.breakpoints.values.lg}px)`;
+export const ipadProScreen = `@media only screen and (max-width: ${theme.breakpoints.values.xl}px)`;
+export const desktopScreen = ` @media only screen and (min-width: ${theme.breakpoints.values.xl}px)`;
