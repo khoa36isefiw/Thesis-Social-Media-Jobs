26b1af7a73534c7cfb7fa6fb7922fdc62f1ae3de
Khoa_HD <huynhdangkhoa961617@gmail.com>
2024-04-19 20:51:14 +0700

    responsive for mobile device

--- client/src/pages/UserHomePage.js
+++ client/src/pages/UserHomePage.js
@@ -4,6 +4,7 @@ import Grid from '@mui/material/Grid';
 import MakePosts from '../components/MakePosts/MakePosts';
 import Post from '../components/MakePosts/Post';
 import Feed from '../components/MakePosts/Feed';
+import { mobileScreen } from '../components/Theme/Theme';
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -19,7 +20,15 @@ function UserHomePage() {
         <Box sx={{ width: '100%' }}>
             <Grid container rowSpacing={3} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
                 <Grid item xs={12}>
-                    <Item sx={{ borderRadius: '12px', mb: 2 }}>
+                    <Item
+                        sx={{
+                            borderRadius: '12px',
+                            mb: 2,
+                            [mobileScreen]: {
+                                borderRadius: 0,
+                            },
+                        }}
+                    >
                         <MakePosts />
                     </Item>
 
