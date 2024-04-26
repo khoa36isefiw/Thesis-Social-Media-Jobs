9206f674aee1f9f3ec491d0e7818faf9a5f6d1d4
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-04-26 23:11:39 +0700

    make responsive with mobile, tablet and ipad pro for notifications page

--- client/src/pages/UserNotifications.js
+++ client/src/pages/UserNotifications.js
@@ -1,26 +1,6 @@
-import {
-    Box,
-    Container,
-    Typography,
-    Button,
-    IconButton,
-    Avatar,
-    Grid,
-    Paper,
-    styled,
-} from '@mui/material';
+import { Box } from '@mui/material';
 import React from 'react';
 import FilterNotifications from '../components/Notifications/FilterNotifications';
-import ShowNotifications from '../components/Notifications/ShowNotifications';
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    // textAlign: 'center',
-    color: theme.palette.text.secondary,
-    borderRadius: '8px',
-    marginBottom: '16px',
-}));
 
 function UserNotifications() {
     return (
