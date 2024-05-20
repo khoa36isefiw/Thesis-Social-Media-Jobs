83fbc13e0e1edf5656aedbfdfcfddbddb46c1edc
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-05-20 16:43:01 +0700

    call reducer

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,14 @@
 // store.js
 import { legacy_createStore as createStore, combineReducers } from 'redux';
 import { sendReducer } from './ButtonSendMessage/sendReducer';
+import { highlightPersonReducer } from './ImportantPerson/highlightPersonReducer';
+import { showMessageReducer } from './ShowMesssage/showMessageReducer';
 
 const rootReducer = combineReducers({
     buttonSendMessage: sendReducer,
+    // starred users
+    importantPerson: highlightPersonReducer,
+    messages: showMessageReducer,
 });
 
 const store = createStore(rootReducer);
