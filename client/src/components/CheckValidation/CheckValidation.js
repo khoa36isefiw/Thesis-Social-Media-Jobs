40c59603a006feb0621b107c1133e9be76a58a13
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-07-05 15:02:29 +0700

    re-define check email check validation

--- client/src/components/CheckValidation/CheckValidation.js
+++ client/src/components/CheckValidation/CheckValidation.js
@@ -1,9 +1,42 @@
 export const validateEmail = (email) => {
-    const specialCharRegex = /^[!@#$%^&*()?":{}|<>`~]/;
+    const specialCharList = [
+        '!',
+        '#',
+        '$',
+        '%',
+        '^',
+        '&',
+        '*',
+        '(',
+        ')',
+        '?',
+        ':',
+        '{',
+        '}',
+        '|',
+        '<',
+        '>',
+        '`',
+        '~',
+    ];
+    const specialChars = [];
 
+    // check for special characters in email except "@" and "."
+    for (let char of email) {
+        if (specialCharList.includes(char) && !['@', '.'].includes(char)) {
+            specialChars.push(char);
+        }
+    }
+
+    if (specialChars.length > 0) {
+        return `Email must not contain special characters: ${specialChars.join(', ')}`;
+    }
+
+    // other validations
     if (!email || typeof email !== 'string') return 'Email is required';
     if (!email.includes('@')) return 'Email must include @';
-    if (specialCharRegex.test(email)) return 'Email must not start with special characters';
+    if (specialCharList.includes(email.charAt(0)))
+        return 'Email must not start with special characters';
     if (/^\d/.test(email)) return 'Email must not start with a number';
 
     return '';
