19c6155c748d580f668c0d3b9b96ee65d88b0c36
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-07-06 16:46:48 +0700

    get and save birthday is selected

--- client/src/redux/ManageAccount/manageAccountAction.js
+++ client/src/redux/ManageAccount/manageAccountAction.js
@@ -1,6 +1,32 @@
-import { SAVE_ACCOUNT_SIGNUP } from '../actionConstant';
+import {
+    GET_BIRTH_DATE,
+    GET_DAY,
+    GET_MONTH,
+    GET_YEAR,
+    SAVE_ACCOUNT_SIGNUP,
+} from '../actionConstant';
 
 export const saveAccountRegistered = (account) => ({
     type: SAVE_ACCOUNT_SIGNUP,
     payload: { account },
+});
+
+export const getBirthDate = (birthday) => ({
+    type: GET_BIRTH_DATE,
+    payload: { birthday },
+});
+
+export const getDay = (day) => ({
+    type: GET_DAY,
+    payload: { day },
+});
+
+export const getMonth = (month) => ({
+    type: GET_MONTH,
+    payload: { month },
+});
+
+export const getYear = (year) => ({
+    type: GET_YEAR,
+    payload: { year },
 });
--- client/src/redux/ManageAccount/manageAccountReducer.js
+++ client/src/redux/ManageAccount/manageAccountReducer.js
@@ -1,6 +1,12 @@
-import { SAVE_ACCOUNT_SIGNUP } from '../actionConstant';
+import {
+    GET_BIRTH_DATE,
+    GET_DAY,
+    GET_MONTH,
+    GET_YEAR,
+    SAVE_ACCOUNT_SIGNUP,
+} from '../actionConstant';
 
-const initialState = { accountsList: [] };
+const initialState = { accountsList: [], birthDate: '', day: '', month: '', year: '' };
 
 export const manageAccountReducer = (state = initialState, action) => {
     switch (action.type) {
@@ -10,6 +16,33 @@ export const manageAccountReducer = (state = initialState, action) => {
             return {
                 ...state,
                 accountsList: [...state.accountsList, account],
+            };
+
+        case GET_BIRTH_DATE:
+            const { birthday } = action.payload;
+            return {
+                ...state,
+                birthDate: birthday,
+            };
+
+        case GET_DAY:
+            const { day } = action.payload;
+            return {
+                ...state,
+                day: day,
+            };
+
+        case GET_MONTH:
+            const { month } = action.payload;
+            return {
+                ...state,
+                month: month,
+            };
+        case GET_YEAR:
+            const { year } = action.payload;
+            return {
+                ...state,
+                year: year,
             };
 
         default:
