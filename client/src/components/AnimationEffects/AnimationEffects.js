89d2a92b19f46948364b3a0b24399a8143643695
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-07-08 10:14:52 +0700

    create object to define animations

---
+++ client/src/components/AnimationEffects/AnimationEffects.js
@@ -0,0 +1,11 @@
+export const modalAnimation = {
+    '@keyframes fadeInScale': {
+        '0%': { opacity: 0, transform: 'scale(0.9)' },
+        '100%': { opacity: 1, transform: 'scale(1)' },
+    },
+    '@keyframes fadeInSlideInTop': {
+        '0%': { opacity: 0, transform: 'translateY(-30px)' },
+        '100%': { opacity: 1, transform: 'translateY(0)' },
+    },
+    animation: 'fadeInSlideInTop 0.5s ease-in-out',
+};
