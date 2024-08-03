d4196c5d73956d84e362a0fd42755aaef365f91c
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-08-03 15:40:47 +0700

    make reaction detail list for each icon

--- client/src/components/MakePosts/ReactionsDetailList.js
+++ client/src/components/MakePosts/ReactionsDetailList.js
@@ -9,18 +9,52 @@ import Love from '../../assets/images/love.png';
 import Laugh from '../../assets/images/laughing.png';
 import UserImage from '../../assets/images/avatar.jpeg';
 import { TabPanel } from '../TabPanel/TabPanel';
-import ShowUserInterestCompaniesAndSchools from '../ShowUserInterestCompaniesAndSchools/ShowUserInterestCompaniesAndSchools';
-import { companiesData, schoolData } from '../CompaniesIsFollowing/data';
+
+const reactionsList = [
+    { reactionImage: Liked, reactionText: 'Liked', numberOfReaction: '123' },
+    { reactionImage: Love, reactionText: 'Loved', numberOfReaction: '345' },
+    { reactionImage: Laugh, reactionText: 'Laughed', numberOfReaction: '101' },
+];
+
+const listAllUsersReaction = [
+    {
+        userReactionIcon: Liked,
+        userNameReacted: 'Luna Kei',
+        userReactedImage: UserImage,
+        userReactedPosition: 'Front-End Developer',
+    },
+    {
+        userReactionIcon: Laugh,
+        userNameReacted: 'Harris',
+        userReactedImage:
+            'https://builtin.com/sites/www.builtin.com/files/styles/og/public/2024-03/Blockchain%20Technology.jpg',
+        userReactedPosition: 'Blockchain Developer',
+    },
+    {
+        userReactionIcon: Love,
+        userNameReacted: 'Luan Phan',
+        userReactedImage:
+            'https://verpex.com/assets/uploads/images/blog/How-to-become-a-Backend-Developer.jpg?v=1665484477',
+        userReactedPosition: 'Back-End Developer',
+    },
+    {
+        userReactionIcon: Laugh,
+        userNameReacted: 'Thoai Huynh',
+        userReactedImage:
+            'https://media.geeksforgeeks.org/wp-content/cdn-uploads/20190626123927/untitlsssssed.png',
+        userReactedPosition: 'Fullstack Developer',
+    },
+];
+
 function ReactionsDetailList({ onCloseReactionsListModal }) {
-    const reactionsList = [
-        { reactionImage: Liked, reactionText: 'Liked', numberOfReaction: '123' },
-        { reactionImage: Love, reactionText: 'Loved', numberOfReaction: '345' },
-        { reactionImage: Laugh, reactionText: 'Laughed', numberOfReaction: '101' },
-    ];
     const [value, setValue] = useState(0);
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
+    };
+
+    const filterUsersByReaction = (reactionText) => {
+        return listAllUsersReaction.filter((user) => user.userReactionIcon === reactionText);
     };
     return (
         <Box
@@ -48,6 +82,7 @@ function ReactionsDetailList({ onCloseReactionsListModal }) {
                 },
             }}
         >
+            {/* show list tabs of reaction */}
             <Box sx={{}}>
                 <Box
                     sx={{
@@ -92,7 +127,7 @@ function ReactionsDetailList({ onCloseReactionsListModal }) {
                         }}
                         label={
                             <Typography sx={{ fontSize: '14px', textTransform: 'capitalize' }}>
-                                All <span>123</span>
+                                All <span>{listAllUsersReaction.length}</span>
                             </Typography>
                         }
                     />
@@ -131,21 +166,21 @@ function ReactionsDetailList({ onCloseReactionsListModal }) {
                     ))}
                 </Tabs>
             </Box>
+
             {/* list user for each reaction */}
             <Box sx={{ overflow: 'scroll', px: 4, height: '480px' }}>
                 <TabPanel value={value} index={0}>
                     {/* Tab 1 Content */}
-                    <ListUsersReactionDetail />
+                    <ListUsersReactionDetail users={listAllUsersReaction} />
                 </TabPanel>
-                <TabPanel value={value} index={1}>
-                    <ShowUserInterestCompaniesAndSchools listData={companiesData} />
-                </TabPanel>
-                <TabPanel value={value} index={2}>
-                    <ShowUserInterestCompaniesAndSchools listData={schoolData} />
-                </TabPanel>
-                <TabPanel value={value} index={3}>
-                    <ListUsersReactionDetail />
-                </TabPanel>
+                {/* get  reactionImage or reaction image type for reaction detail */}
+                {reactionsList.map((reaction, index) => (
+                    <TabPanel value={value} index={index + 1} key={index}>
+                        <ListUsersReactionDetail
+                            users={filterUsersByReaction(reaction.reactionImage)}
+                        />
+                    </TabPanel>
+                ))}
             </Box>
         </Box>
     );
@@ -153,39 +188,10 @@ function ReactionsDetailList({ onCloseReactionsListModal }) {
 
 export default ReactionsDetailList;
 
-const ListUsersReactionDetail = () => {
-    const listAllUsersReaction = [
-        {
-            userReactionIcon: Liked,
-            userNameReacted: 'Luna Kei',
-            userReactedImage: UserImage,
-            userReactedPosition: 'Front-End Developer',
-        },
-        {
-            userReactionIcon: Laugh,
-            userNameReacted: 'Harris',
-            userReactedImage:
-                'https://builtin.com/sites/www.builtin.com/files/styles/og/public/2024-03/Blockchain%20Technology.jpg',
-            userReactedPosition: 'Blockchain Developer',
-        },
-        {
-            userReactionIcon: Love,
-            userNameReacted: 'Luan Phan',
-            userReactedImage:
-                'https://verpex.com/assets/uploads/images/blog/How-to-become-a-Backend-Developer.jpg?v=1665484477',
-            userReactedPosition: 'Back-End Developer',
-        },
-        {
-            userReactionIcon: Laugh,
-            userNameReacted: 'Thoai Huynh',
-            userReactedImage:
-                'https://media.geeksforgeeks.org/wp-content/cdn-uploads/20190626123927/untitlsssssed.png',
-            userReactedPosition: 'Fullstack Developer',
-        },
-    ];
+const ListUsersReactionDetail = ({ users }) => {
     return (
         <React.Fragment>
-            {listAllUsersReaction.map((user, index) => (
+            {users.map((user, index) => (
                 <>
                     <Box sx={{ display: 'flex', alignItems: 'center', py: 1, cursor: 'pointer' }}>
                         {/* <Avatar src={UserImage} sx={{ height: '64px', width: '64px' }} /> */}
@@ -194,19 +200,6 @@ const ListUsersReactionDetail = () => {
                                 height: '56px',
                                 width: '56px',
                                 position: 'relative',
-
-                                // [mobileScreen]: {
-                                //     height: '50px',
-                                //     width: '50px',
-                                // },
-                                // [tabletScreen]: {
-                                //     height: '60px',
-                                //     width: '60px',
-                                // },
-                                // [ipadProScreen]: {
-                                //     height: '70px',
-                                //     width: '70px',
-                                // },
                             }}
                         >
                             <Avatar
@@ -248,9 +241,218 @@ const ListUsersReactionDetail = () => {
                         </Box>
                     </Box>
                     {/* don't show divider for the last user */}
-                    {index !== listAllUsersReaction.length - 1 && <Divider />}
+                    {index !== users.length - 1 && <Divider />}
                 </>
             ))}
         </React.Fragment>
     );
 };
+
+// const reactionsList = [
+//     { reactionImage: Liked, reactionText: 'Liked', numberOfReaction: 123 },
+//     { reactionImage: Love, reactionText: 'Loved', numberOfReaction: 345 },
+//     { reactionImage: Laugh, reactionText: 'Laughed', numberOfReaction: 101 },
+// ];
+
+// const listAllUsersReaction = [
+//     {
+//         userReactionIcon: Liked,
+//         userNameReacted: 'Luna Kei',
+//         userReactedImage: UserImage,
+//         userReactedPosition: 'Front-End Developer',
+//     },
+//     {
+//         userReactionIcon: Laugh,
+//         userNameReacted: 'Harris',
+//         userReactedImage:
+//             'https://builtin.com/sites/www.builtin.com/files/styles/og/public/2024-03/Blockchain%20Technology.jpg',
+//         userReactedPosition: 'Blockchain Developer',
+//     },
+//     {
+//         userReactionIcon: Love,
+//         userNameReacted: 'Luan Phan',
+//         userReactedImage:
+//             'https://verpex.com/assets/uploads/images/blog/How-to-become-a-Backend-Developer.jpg?v=1665484477',
+//         userReactedPosition: 'Back-End Developer',
+//     },
+//     {
+//         userReactionIcon: Laugh,
+//         userNameReacted: 'Thoai Huynh',
+//         userReactedImage:
+//             'https://media.geeksforgeeks.org/wp-content/cdn-uploads/20190626123927/untitlsssssed.png',
+//         userReactedPosition: 'Fullstack Developer',
+//     },
+// ];
+
+// const ListUsersReactionDetail = ({ users }) => {
+//     return (
+//         <React.Fragment>
+//             {users.map((user, index) => (
+//                 <React.Fragment key={index}>
+//                     <Box sx={{ display: 'flex', alignItems: 'center', py: 1, cursor: 'pointer' }}>
+//                         <Box sx={{ height: '56px', width: '56px', position: 'relative' }}>
+//                             <Avatar
+//                                 src={user.userReactedImage}
+//                                 alt={'UserImage'}
+//                                 sx={{
+//                                     width: '100%',
+//                                     height: '100%',
+//                                     zIndex: 2,
+//                                     '&:hover': {
+//                                         cursor: 'pointer',
+//                                     },
+//                                     objectFit: 'cover',
+//                                 }}
+//                             />
+//                             <Box
+//                                 component={'img'}
+//                                 src={user.userReactionIcon}
+//                                 sx={{
+//                                     position: 'absolute',
+//                                     height: '20px',
+//                                     width: '20px',
+//                                     backgroundColor: 'green',
+//                                     right: 0,
+//                                     bottom: 0,
+//                                     borderRadius: '50%',
+//                                     border: '2px solid white',
+//                                     zIndex: 3,
+//                                 }}
+//                             />
+//                         </Box>
+//                         <Box sx={{ ml: 1 }}>
+//                             <Typography fontWeight="bold">{user.userNameReacted}</Typography>
+//                             <Typography fontSize="13px">{user.userReactedPosition}</Typography>
+//                         </Box>
+//                     </Box>
+//                     {index !== users.length - 1 && <Divider />}
+//                 </React.Fragment>
+//             ))}
+//         </React.Fragment>
+//     );
+// };
+
+// function ReactionsDetailList({ onCloseReactionsListModal }) {
+//     const [value, setValue] = useState(0);
+
+//     const handleChange = (event, newValue) => {
+//         setValue(newValue);
+//     };
+
+//     const filterUsersByReaction = (reactionText) => {
+//         return listAllUsersReaction.filter((user) => user.userReactionIcon === reactionText);
+//     };
+
+//     return (
+//         <Box
+//             sx={{
+//                 position: 'relative',
+//                 backgroundColor: 'white',
+//                 maxWidth: '600px',
+//                 height: '600px',
+//                 margin: 'auto',
+//                 mt: '64px',
+//                 borderRadius: '12px',
+//                 boxShadow: '0 4px 4px #333',
+//                 overflow: 'hidden',
+//             }}
+//         >
+//             <Box sx={{}}>
+//                 <Box
+//                     sx={{
+//                         display: 'flex',
+//                         justifyContent: 'space-between',
+//                         alignItems: 'center',
+//                         p: 1,
+//                     }}
+//                 >
+//                     <Typography fontSize="20px" fontWeight="bold" sx={{ mx: 2, flexGrow: 2 }}>
+//                         Reactions
+//                     </Typography>
+//                     <IconButton
+//                         disableFocusRipple
+//                         sx={{
+//                             mx: '2px',
+//                             '&:hover': {
+//                                 backgroundColor: '#d9d9d9',
+//                             },
+//                         }}
+//                         onClick={onCloseReactionsListModal}
+//                     >
+//                         <CloseIcon sx={{ fontSize: '28px' }} />
+//                     </IconButton>
+//                 </Box>
+//                 <Tabs
+//                     value={value}
+//                     onChange={handleChange}
+//                     sx={{
+//                         borderBottom: '1px solid #333',
+//                         p: 0,
+//                         '.MuiTabs-indicator': {
+//                             height: '4px',
+//                         },
+//                     }}
+//                 >
+//                     <Tab
+//                         sx={{
+//                             '&:hover': {
+//                                 backgroundColor: 'rgba(0, 0, 0, 0.1)',
+//                             },
+//                         }}
+//                         label={
+//                             <Typography sx={{ fontSize: '14px', textTransform: 'capitalize' }}>
+//                                 All <span>{listAllUsersReaction.length}</span>
+//                             </Typography>
+//                         }
+//                     />
+//                     {reactionsList.map((reaction, index) => (
+//                         <Tab
+//                             key={index}
+//                             sx={{
+//                                 display: 'flex',
+//                                 alignItems: 'center',
+//                                 justifyContent: 'space-between',
+//                                 '&:hover': {
+//                                     backgroundColor: 'rgba(0, 0, 0, 0.1)',
+//                                 },
+//                             }}
+//                             label={
+//                                 <Box sx={{ display: 'flex', alignItems: 'center' }}>
+//                                     <Box
+//                                         component="img"
+//                                         src={reaction.reactionImage}
+//                                         alt={reaction.reactionText}
+//                                         sx={{ height: '24px', width: '24px' }}
+//                                     />
+//                                     <Typography
+//                                         sx={{
+//                                             fontSize: '14px',
+//                                             textTransform: 'capitalize',
+//                                             ml: '4px',
+//                                         }}
+//                                     >
+//                                         {reaction.numberOfReaction}
+//                                     </Typography>
+//                                 </Box>
+//                             }
+//                         />
+//                     ))}
+//                 </Tabs>
+//             </Box>
+//             <Box sx={{ overflowY: 'scroll', px: 4, height: '480px' }}>
+//                 <TabPanel value={value} index={0}>
+//                     <ListUsersReactionDetail users={listAllUsersReaction} />
+//                 </TabPanel>
+//                 {reactionsList.map((reaction, index) => (
+//                     <TabPanel value={value} index={index + 1} key={index}>
+//                         <ListUsersReactionDetail
+//                             users={filterUsersByReaction(reaction.reactionImage)}
+//                         />
+//                     </TabPanel>
+//                 ))}
+//             </Box>
+//         </Box>
+//     );
+// }
+
+// export default ReactionsDetailList;
